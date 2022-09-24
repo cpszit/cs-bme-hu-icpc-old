00b3ed93aed9f5a465a095d81b8353f057f9e8fc
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-09-24 19:50:41 +0200

    Cleanup

--- gallery/errors/configure_help.php
+++
@@ -1,2 +0,0 @@
-If you experience problems, you should seek help on the
-<a href="http://gallery.sourceforge.net/help.php">Gallery Help Page</a>
--- gallery/secure.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo IF THIS FILE EXISTS, YOU CAN'T CONFIGURE GALLERY > setup\SECURE
-
-echo : 
-echo : Your Gallery is now secure and cannot be configured.  If
-echo : you wish to reconfigure it, run the configure.bat script.
-echo : 
